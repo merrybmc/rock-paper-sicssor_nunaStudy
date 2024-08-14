b15c7604aa84b278b20f3939f89946d1fe4b06a2
Byeong Min Cho <merrybmc@gmail.com>
2024-08-14 18:53:21 +0900

    feat : result

--- src/App.js
+++ src/App.js
@@ -23,11 +23,13 @@ const choice = {
 function App() {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState('');
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerCoice = randomChoice();
     setComputerSelect(computerCoice);
+    judgement(choice[userChoice], computerCoice);
   };
 
   const randomChoice = () => {
@@ -36,10 +38,21 @@ function App() {
     return choice[randomItem];
   };
 
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return setResult('tie');
+    } else if (user.name === 'Rock')
+      return computer.name === 'Scissors' ? setResult('win') : setResult('lose');
+    else if (user.name === 'Sicssors')
+      return computer.name === 'Paper' ? setResult('win') : setResult('lose');
+    else if (user.name === 'Paper')
+      return computer.name === 'Rock' ? setResult('win') : setResult('lose');
+  };
+
   return (
     <div className='main'>
-      <Box title='You' item={userSelect} />
-      <Box title='Computer' item={computerSelect} />
+      <Box title='You' item={userSelect} result={result} />
+      <Box title='Computer' item={computerSelect} result={result} />
       <div>
         <button onClick={() => play('sicssors')}>가위</button>
         <button onClick={() => play('rock')}>바위</button>
--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Box({ title, item }) {
-  console.log(item);
+export default function Box({ title, item, result }) {
+  const [computerResult, setComputerResult] = useState(null);
+
+  useEffect(() => {
+    result === 'tie' && setComputerResult('tie');
+    result === 'win' && setComputerResult('lose');
+    result === 'lose' && setComputerResult('win');
+  }, [result]);
+
   return (
     <div className='box'>
       <h1>{title}</h1>
       <img className='item-img' src={item && item.img} alt='' />
-      <h2>WIN</h2>
+      <h2>{title === 'You' ? result : computerResult}</h2>
     </div>
   );
 }
