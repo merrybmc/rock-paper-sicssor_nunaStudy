891931569c84588e6d4a5169a8b770a1121bdb7a
Byeong Min Cho <merrybmc@gmail.com>
2024-08-15 19:16:09 +0900

    feat : class component 변환

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,24 +1,69 @@
 import React, { Component } from 'react';
+import BoxClass from './component/BoxClass';
+import sicssors from './asset/pika_v.png';
+import rock from './asset/pika_rock.png';
+import paper from './asset/pika_paper.png';
+
+const choice = {
+  rock: {
+    name: 'Rock',
+    img: rock,
+  },
+  scissors: {
+    name: 'Scissors',
+    img: sicssors,
+  },
+  paper: {
+    name: 'Paper',
+    img: paper,
+  },
+};
 
 export default class AppClass extends Component {
-  constructor(props) {
-    // 생성자 컴포넌트
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      counter2: 0,
-      num: 1,
-      value: 0,
-    };
-
-    increase = () => {
-      this.setState({ counter2: this.state.counter2 + 1, value: this.state.value + 1 });
+      userSelect: null,
+      computerSelect: null,
+      result: '',
     };
   }
+
+  play = (userChoice) => {
+    let computerChoice = this.randomChoice();
+    this.setState({
+      userSelect: choice[userChoice],
+      computerSelect: computerChoice,
+      result: this.judgement(choice[userChoice], computerChoice),
+    });
+  };
+  randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return 'tie';
+    } else if (user.name === 'Rock') return computer.name === 'Scissors' ? 'win' : 'lose';
+    else if (user.name === 'Scissors') return computer.name === 'Paper' ? 'win' : 'lose';
+    else if (user.name === 'Paper') return computer.name === 'Rock' ? 'win' : 'lose';
+  };
+
   render() {
     return (
-      <div>
-        <div>state:{this.state.counter2}</div>
-        <button onClick={this.increase}>클릭!</button>
+      <div className='main'>
+        <h1>Rock-Paper-Sicssors</h1>
+        <div className='box-Group'>
+          <BoxClass title='You' item={this.state.userSelect} result={this.state.result} />
+          <BoxClass title='Computer' item={this.state.computerSelect} result={this.state.result} />
+        </div>
+        <div className='button-Group'>
+          <button onClick={() => this.play('scissors')}>가위</button>
+          <button onClick={() => this.play('rock')}>바위</button>
+          <button onClick={() => this.play('paper')}>보</button>
+        </div>
       </div>
     );
   }
